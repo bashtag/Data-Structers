b42fe4b57a5ef5a9497548fc80032a1da4598f55
bashtag <btstsiy@hotmail.com>
2023-04-04 16:14:34 +0300

    my test class to test ldlinkedlist

--- 2022/Homework-3 (LinkedList)/driver.java
+++ 2022/Homework-3 (LinkedList)/driver.java
@@ -3,14 +3,18 @@ import java.util.*;
 public class driver {
 	public static void main(String[] args) {
 		LDLinkedList<Integer>	ownList = new LDLinkedList<Integer>();
-		LinkedList<Integer>	deneme = new LinkedList<Integer>();
-
-
 		ownList.add(15);
 		ownList.add(32);
 		ownList.add(11);
 		ownList.add(156);
+		ownList.add(32);
 		ownList.add(134);
+		ownList.set(5, 128);
+		ownList.add(3, 42);
+
+		Object[]	oArray = ownList.toArray();
+		Integer[]	iArray = ownList.toArray(new Integer[0]);
+		ownList.remove(Integer.valueOf(42));
 
 		System.out.println("olabilir");
 		System.out.println(ownList.get(0));
@@ -18,15 +22,35 @@ public class driver {
 		System.out.println(ownList.get(2));
 		System.out.println(ownList.get(3));
 		System.out.println(ownList.get(4));
+		System.out.println(ownList.get(5));
 		System.out.println("----------------------");
-		System.out.print(ownList.contains(15));
+		System.out.println(ownList.contains(31));
+		System.out.println(ownList.indexOf(31));
+		System.out.println(ownList.indexOf(32));
+		System.out.println(ownList.lastIndexOf(32));
 
 		System.out.println("-------------------------");
+
+		ownList.removeFirst();
+		// ownList.removeLast();
+		ownList.add(69);
 
 		Iterator<Integer>	iter = ownList.descendingIterator();
 
 		while (iter.hasNext()) {
 			System.out.println(iter.next());
 		}
+
+		System.out.println("---------------------------------------");
+
+		for (Object o : oArray) {
+			System.out.print(o + ", ");
+		}
+		System.out.println();
+
+		for (Integer he : iArray) {
+			System.out.print(he + ", ");
+		}
+		System.out.println();
 	}
 }
