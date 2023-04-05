c17f18bcb16cc4d962096909900afdf20ed4d442
bashtag <btstsiy@hotmail.com>
2023-04-05 11:37:07 +0300

    javadoc is uploaded

---
+++ 2022/Homework-3 (LinkedList)/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LDLinkedList.DescendingIterator"},{"p":"<Unnamed>","l":"LDLinkedList"},{"p":"<Unnamed>","l":"LDLinkedList.Node"}];updateSearchResults();
