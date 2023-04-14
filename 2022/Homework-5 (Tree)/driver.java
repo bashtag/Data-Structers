4462392acf38c9079130de8a2809e59744997d4f
bashtag <btstsiy@hotmail.com>
2023-04-14 23:42:11 +0300

    test method for bst

--- 2022/Homework-5 (Tree)/driver.java
+++ 2022/Homework-5 (Tree)/driver.java
@@ -3,8 +3,12 @@ import java.util.Arrays;
 public class driver {
 	
 	public static void main(String[] args) throws Exception {
+		testBst();
+	}
+	
+	private static void	testBst() throws Exception{
 		BinarySearchTree<Integer>	bst0 = new BinarySearchTree<Integer>();
-
+	
 		bst0.insert(5);
 		bst0.insert(15);
 		bst0.insert(29);
@@ -15,7 +19,7 @@ public class driver {
 		bst0.insert(9);
 		bst0.insert(6);
 		bst0.insert(13);
-
+	
 		System.out.println(bst0);
 		System.out.println("---------------------------------");
 		
@@ -24,12 +28,12 @@ public class driver {
 		System.out.println("---------------------------------");
 		bst0.delete(100);
 		System.out.println(bst0);
-
+	
 		String myst = bst0.serialize();
 		String[] arr = myst.split(" ");
 		System.out.println(Arrays.toString(arr));
 		System.out.println("----------DES_PART----------------");
-
+	
 		BinarySearchTree<Integer>	deserialize = BinarySearchTree.deserialize(myst);
 		System.out.println(deserialize);
 	}
