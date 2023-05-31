14805428b3b848140d57d7887fe3de8fabd40f15
bashtag <btstsiy@hotmail.com>
2023-05-31 16:39:34 +0300

    Graph structure skeleton

---
+++ 2022/Homework-8 (Graph)/CustomGraph.java
@@ -0,0 +1,95 @@
+import java.util.Iterator;
+
+public class CustomGraph implements DynamicGraph {
+
+	@Override
+	public int getNumV() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getNumV'");
+	}
+
+	@Override
+	public boolean isDirected() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'isDirected'");
+	}
+
+	@Override
+	public void insert(Edge edge) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'insert'");
+	}
+
+	@Override
+	public boolean isEdge(int source, int dest) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'isEdge'");
+	}
+
+	@Override
+	public Edge getEdge(int source, int dest) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getEdge'");
+	}
+
+	@Override
+	public Iterator<Edge> edgeIterator(int source) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'edgeIterator'");
+	}
+
+	@Override
+	public Vertex newVertex(String label, double weight) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'newVertex'");
+	}
+
+	@Override
+	public boolean addVertex(Vertex newVertex) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'addVertex'");
+	}
+
+	@Override
+	public boolean addEdge(int vertexId1, int verrtexId2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'addEdge'");
+	}
+
+	@Override
+	public boolean removeEdge(int vertexId1, int vertexId2) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'removeEdge'");
+	}
+
+	@Override
+	public boolean removeVertex(int vertexId) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'removeVertex'");
+	}
+
+	@Override
+	public boolean removeVertex(String label) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'removeVertex'");
+	}
+
+	@Override
+	public DynamicGraph filterVertices(String key, String filter) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'filterVertices'");
+	}
+
+	@Override
+	public double[][] exportMatrix() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'exportMatrix'");
+	}
+
+	@Override
+	public void printGraph() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'printGraph'");
+	}
+	
+}
