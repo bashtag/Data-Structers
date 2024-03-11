0577fdae8b88a833b470b7f87ade5f00bef7b267
bashtag <btstsiy@hotmail.com>
2024-03-11 19:28:03 +0300

    folder name

--- 2023/Homework-1 (Not Any DS)/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	static final int MAX_RECORDS = 1000; // Assuming maximum records
-
-	public static void main(String[] args) {
-		file_io fileIO = new file_io();
-
-		fileIO.startProcess();
-	}
-}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data-Structers
 CSE-222 lesson homeworks and some notes given by Fatih Erdoğan Sevilgen
+
+Homeworks have been given by Gökhan Kaya for 2024
